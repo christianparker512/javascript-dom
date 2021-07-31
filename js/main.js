26b50b893a4cf1a3f946948cfcb98b98c05f9721
Christian Parker <christianparker512@gmail.com>
2021-07-31 16:33:22 -0400

    main

--- js/main.js
+++ js/main.js
@@ -1,2 +1,7 @@
 'use strict';
 
+const myEle1 = document.querySelector('.myEle');
+const myEle2 = document.querySelector('.myId');
+const myEle3 = document.querySelector('.div')
+
+const myEle1.textContent = "hello world";
